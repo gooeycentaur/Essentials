7ebfae2757dafe2e8d363f02a70442ac732a4817
gooeycentaur <gaven.wayne@outlook.com>
2026-04-04 11:14:22 -0600

    Uploaded grass from the unity assets
    I added grass and bushes to help it feel over grown and a little more alive, refined some hit boxes and had someone test play it.

---
+++ DAGV1700/AdventureGame/Assets/2d Grass Sprites/Read me.cs
@@ -0,0 +1,4 @@
+// -1) The Sprites are designed to work with unity terrain tools to paint them as grass.
+// -2) They can be used even as sprites grass as simple gameobjects on the ground.
+// -3) They are not animated and dont have any grass shader material.--> More Textures and other features Coming soon.
+//Thank you very much for using my Textures.
