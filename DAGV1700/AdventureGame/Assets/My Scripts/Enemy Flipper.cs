80331246340ae161633b25155c9e8dfc821514fe
gooeycentaur <gaven.wayne@outlook.com>
2026-04-08 17:49:01 -0600

    Unit12

---
+++ DAGV1700/AdventureGame/Assets/My Scripts/Enemy Flipper.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFlipper : MonoBehaviour 
+{
+    private NavMeshAgent  agent;
+    private SpriteRenderer  spriteRenderer;
+    
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+    void Update()
+    {
+        float xVel = agent.velocity.x;
+
+        if (Mathf.Abs(xVel) > 0.01f)
+        {
+            spriteRenderer.flipX = xVel < 0f;
+        }
+    }
+}
